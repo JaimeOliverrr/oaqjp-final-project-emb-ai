1814550214e81346f956cd7269b6c22a8d07aff3
JaimeOliverrr <olivetgerman@gmail.com>
2024-07-23 10:33:45 -0400

    6th/Final Commit

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+'''
+Emotion detector function in web application development.
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,16 +8,24 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emot_detector():
+    '''
+    Calling emotion detector function through the web application.
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-    if response['dominant_emotion'] == None:
+    if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
-    response_string =  ', '.join(f"'{k}': {v}" for k, v in response.items() if k != 'dominant_emotion')
-    return f"For the given statement, the system response is {response_string}. The dominant emotion is <b>{response['dominant_emotion']}</b>."
+    string =  ', '.join(f"'{k}': {v}" for k, v in response.items() if k != 'dominant_emotion')
+    dominant = response['dominant_emotion']
+    return f"For the given statement, the system response is {string}. The dominant emotion is <b>{dominant}</b>."
 
 @app.route("/")
 def render_index_page():
+    '''
+    Index page.
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
